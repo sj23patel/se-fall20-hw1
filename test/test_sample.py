b2b7fd449e7bc794f0df2107b7ca44e16cb28983
jaymodi98 <jaymodi98@gmail.com>
2020-08-17 13:15:39 -0400

    .

--- test/test_sample.py
+++ test/test_sample.py
@@ -3,7 +3,7 @@ sys.path.append("../")
 import script
 
 def test_should_pass():
-    assert code.inc(3) == 4
+    assert script.inc(3) == 4
 
 def test_should_fail():
-    assert not code.inc(3) == 5
+    assert not script.inc(3) == 5
