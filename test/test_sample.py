dc23d8ef9ea6e9c2aa18757470ee4363c2720535
jaymodi98 <jaymodi98@gmail.com>
2020-08-16 20:35:26 -0400

    modified tests

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,7 +1,10 @@
-import code
+import sys
+sys.path.append("../code")
+# import code
+from mod import inc
 
 def test_should_pass():
     assert code.inc(3) == 4
 
 def test_should_fail():
-    assert code.inc(3) == 5
+    assert not code.inc(3) == 5
